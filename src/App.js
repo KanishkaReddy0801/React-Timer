095c962a0bf7bb7e339a6e1f63eaae9ce2e09650
KanishkaReddy0801 <krishnakanishkareddyalla@gmail.com>
2023-02-14 14:24:19 +0530

    Timer using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [timeCount, setTimeCount] = useState(0);
+  const [countdown, setCountdown] = useState(null);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      const time = Math.floor(parseFloat(event.target.value));
+      if (isNaN(time)) {
+        setTimeCount(0);
+      } else {
+        setTimeCount(time);
+      }
+      event.target.value = "";
+    }
+  };
+
+  useEffect(() => {
+    if (timeCount > 0) {
+      if (countdown) clearInterval(countdown);
+      setCountdown(setInterval(() => {
+        setTimeCount((prevCount) => {
+          const newCount = prevCount - 1;
+          return newCount < 0 ? 0 : newCount;
+        });
+      }, 1000));
+    }
+  }, [timeCount]);
+
+  useEffect(() => {
+    if (timeCount === 0 && countdown) {
+      clearInterval(countdown);
+      setCountdown(null);
+    }
+  }, [timeCount, countdown]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input id="timeCount" onKeyDown={handleKeyDown} />
+      <div id="current-time">{timeCount}</div>
     </div>
   );
 }
